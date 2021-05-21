3a57843280df465f025b417a992539c95a506dfb
kangjf1943 <kangjf1943@gmail.com>
2021-05-21 15:28:26 +0900

    Fix: 结合下游流程修改服务业部分代码

--- Model_service.R
+++ Model_service.R
@@ -1,4 +1,6 @@
 ## 服务业
+com_subsector <- c("electricity", "lpg_and_gas")
+
 ## 历史数据
 # 活动水平：服务业从业人口和GDP
 com_act <- func_read_trans("2VHEE264", "从业人口")
@@ -6,20 +8,26 @@ com_act <- com_act[, c("year", "第三产业")]
 names(com_act)[2] <- "com_employee"
 com_act$com_employee <- com_act$com_employee/10000
 comment(com_act$com_employee) <- "万人"
-com_act <- merge(com_act, gdp[c("year", "#第三产业")], by = "year")
+com_act <- merge(com_act, global_gdp[c("year", "#第三产业")], by = "year")
 names(com_act)[3] <- "com_gdp"
-com_act
-# 活动总量：电力，液化石油气，天然气
-com_nrgsum_ls <- vector("list")
+# 测试
+# func_show_trend(com_act)
+# 能耗总量
+com_nrgsum_ls <- vector("list", 2)
+names(com_nrgsum_ls) <- com_subsector
 # 读取厦门市用电数据
 com_nrgsum_ls[[1]] <- func_read_trans("2I4DKY2A")[, c("year", "##第三产业")]
 names(com_nrgsum_ls[[1]])[2] <- "electricity"
 # 读取厦门市服务业用能
 com_nrgsum_ls[[2]] <- func_read_trans("HV4JBQTQ")
-names(com_nrgsum_ls) <- c("electricity", "lpg_and_gas")
+names(com_nrgsum_ls[[2]])[2:3] <- c("lpg", "gas")
 # 活动强度
 com_nrgintst_ls <- func_nrg_intst_ls(com_nrgsum_ls, com_act)
-func_show_trend(com_act)
+# 测试
+# 问题：2015年用气强度比2014年少
+# func_show_trend_ls(com_nrgsum_ls)
+# func_show_trend_ls(com_nrgintst_ls)
+
 
 ## 预测分析
 # 活动水平
@@ -32,8 +40,9 @@ proj_com_act$com_gdp <- proj_gdp$GDP *
                          value = c(0.60, 0.65, 0.70))$value
 func_history_project_df(com_act, proj_com_act)
 
-# 能耗强度
+# 未来能耗强度
 proj_com_nrgintst_ls <- vector("list")
+names(proj_com_nrgintst_ls) <- com_subsector
 proj_com_nrgintst_ls[[1]] <- 
   func_interp_2(year = c(2019, 2030, 2050), 
                 value = c(2958.967, 2958.967, 2958.967), 
@@ -53,5 +62,6 @@ func_history_project_ls(com_nrgintst_ls, proj_com_nrgintst_ls)
 
 # 能耗总量
 proj_com_nrgsum_ls <- func_nrg_sum_ls(proj_com_nrgintst_ls, proj_com_act)
+names(proj_com_nrgsum_ls) <- com_subsector
 func_history_project_ls(com_nrgsum_ls, proj_com_nrgsum_ls)
 
