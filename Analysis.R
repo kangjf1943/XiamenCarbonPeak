dd4d3d0415dc1f39148175b9442023daee27672a
kangjf1943 <kangjf1943@gmail.com>
2021-06-20 22:34:20 +0900

    Fix: 修改输出各情景能耗和排放量总量及强度数据的代码

--- Analysis.R
+++ Analysis.R
@@ -2234,14 +2234,24 @@ if (set_dataexport == TRUE) {
   exp_var <- 
     prj_global_gdp[c("year", "GDP")][which(prj_global_gdp$year %% 5 == 0), ]
   for (i in set_scalcs) {
+    # 能耗量相关指标
     exp_var[, paste0(i, "_nrg (万吨标煤)")] <- 
       tot_nrgsum_ls[[i]][which(
         tot_nrgsum_ls[[i]]$year %% 5 == 0), ]$energyconsump/10000
     exp_var[, paste0(i, " (吨标煤/万元GDP)")] <- 
       exp_var[, paste0(i, "_nrg (万吨标煤)")]*10000 / exp_var[, "GDP"]
-    exp_var[, paste0(i, " 变化率")] <- 
+    exp_var[, paste0(i, "_nrg变化率")] <- 
       func_ratecalc(exp_var[, c("year", paste0(i, " (吨标煤/万元GDP)"))], 
-                    paste0(i, " (吨标煤/万元GDP)"))$rate
+                    paste0(i, " (吨标煤/万元GDP)"))$rate*100
+    # 排放量相关指标
+    exp_var[, paste0(i, "_emis (万吨)")] <- 
+      tot_emissum_ls[[i]][which(
+        tot_emissum_ls[[i]]$year %% 5 == 0), ]$co2
+    exp_var[, paste0(i, " (吨/万元)")] <- 
+      exp_var[, paste0(i, "_emis (万吨)")]*10000 / exp_var[, "GDP"]
+    exp_var[, paste0(i, "_emis变化率")] <- 
+      func_ratecalc(exp_var[, c("year", paste0(i, " (吨/万元)"))], 
+                    paste0(i, " (吨/万元)"))$rate*100
   }
   if (set_nrgplng_scope == FALSE) {
     func_dataexp("各情景五年GDP和能耗强度", mydata = exp_var)
