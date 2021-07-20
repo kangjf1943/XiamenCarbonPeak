c49b41a70e2ca6fe8ae77e09ba626b132635f583
Kang <kangjf1943@gmail.com>
2021-07-20 16:47:16 +0900

    Fix: 调整减煤情景以改变人均生活能耗五年变化率
    目前调整结果为，接下来三个五年人均生活能耗变化率分别为7.8%、7.6%和7.8%。

--- Analysis.R
+++ Analysis.R
@@ -1,7 +1,7 @@
 # SETTING ----
 # 计算内容或口径相关设置
 # 设置要计算的情景
-set_scalcs <- "BAU_WLC_OTHER"
+set_scalcs <- "BAU_SLC_DECOAL_OTHER"
   # c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
 set_nrgplng_scope <- FALSE # 是否采用能源规划口径
 set_lowdev <- FALSE #是否采用经济低发展情景
@@ -1946,8 +1946,8 @@ for (set_scalc in set_scalcs) {
   } else if (grepl("SLC", set_scalc)) { 
     #### SLC ----
     hh_nrgintst[[set_scalc]][[1]] <- func_interp_3(
-      year = c(2019, 2025, 2030, 2035, 2060), 
-      scale = c(1, 1.05, 1.20, 1.30, 1.4), 
+      year = c(2019, 2025, 2028, 2030, 2035, 2060), 
+      scale = c(1.0, 1.09, 1.19, 1.15, 1.26, 1.4), 
       base = func_lastone(hh_nrgintst[["BY"]][["household"]][, "electricity"]))
   } else if (grepl("WLC", set_scalc)) { 
     #### WLC ----
