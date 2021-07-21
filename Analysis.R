4013eb82e571c3d59281f6a63809af93ee8a356c
Kang <kangjf1943@gmail.com>
2021-07-21 22:15:56 +0900

    Fix: 调整减排情景以改变人均生活能耗和单位GDP能耗五年变化率

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,8 @@
 # SETTING ----
 # 计算内容或口径相关设置
 # 设置要计算的情景
-set_scalcs <- "BAU_SLCPLUS_OTHER"
-  # c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
+set_scalcs <- 
+  c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
 set_nrgplng_scope <- FALSE # 是否采用能源规划口径
 set_lowdev <- FALSE #是否采用经济低发展情景
 
@@ -1879,8 +1879,8 @@ for (set_scalc in set_scalcs) {
   } else if (grepl("WLC", set_scalc)) { 
     #### WLC ----
     com_nrgintst[[set_scalc]][[1]] <- func_interp_3(
-      year = c(2019, 2025, 2028, 2030, 2060), 
-      scale = c(1.0, 1.05, 1.07, 1.04, 0.95), 
+      year = c(2019, 2025, 2028, 2029, 2030, 2035, 2060), 
+      scale = c(1.0, 1.02, 1.07, 1.05, 1.06, 1.10, 0.95), 
       base = func_lastone(com_nrgintst[["BY"]]$electricity$electricity), 
       "electricity", showplot = set_showplot)
     com_nrgintst[[set_scalc]][[2]] <- func_interp_3(
@@ -2016,7 +2016,7 @@ for (set_scalc in set_scalcs) {
     #### WLC ----
     hh_nrgintst[[set_scalc]][[1]] <- func_interp_3(
       year = c(2019, 2025, 2028, 2030, 2035, 2060), 
-      scale = c(1.0, 1.11, 1.19, 1.19, 1.31, 1.45), 
+      scale = c(1.0, 1.11, 1.21, 1.24, 1.35, 1.45), 
       base = func_lastone(hh_nrgintst[["BY"]][["household"]][, "electricity"]), 
       showplot = set_showplot)
   } else { 
