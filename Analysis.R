27211945341139ece588045b8f322a3d2c45384d
Kang <kangjf1943@gmail.com>
2021-07-24 20:36:03 +0900

    fix: 更新GDP后调整强化情景参数

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,8 @@
 # SETTING ----
 # 计算内容或口径相关设置
 # 设置要计算的情景
-set_scalcs <- 
-  c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
+set_scalcs <- "BAU_SLCPLUS_OTHER"
+  # c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
 set_nrgplng_scope <- FALSE # 是否采用能源规划口径
 set_lowdev <- FALSE #是否采用经济低发展情景
 
@@ -1842,7 +1842,7 @@ for (set_scalc in set_scalcs) {
     #### PLUS ----
     com_nrgintst[[set_scalc]][[1]] <- func_interp_3(
       year = c(2019, 2025, 2028, 2030, 2035, 2060), 
-      scale = c(1.0, 1.00, 0.97, 0.99, 1.02, 0.9), 
+      scale = c(1.0, 1.03, 0.97, 0.99, 1.02, 0.9), 
       base = func_lastone(com_nrgintst[["BY"]]$electricity$electricity), 
       "electricity", showplot = set_showplot)
     com_nrgintst[[set_scalc]][[2]] <- func_interp_3(
@@ -1994,7 +1994,7 @@ for (set_scalc in set_scalcs) {
     #### PLUS ----
     hh_nrgintst[[set_scalc]][[1]] <- func_interp_3(
       year = c(2019, 2025, 2028, 2030, 2035, 2060), 
-      scale = c(1.0, 1.04, 1.03, 1.07, 1.21, 1.30), 
+      scale = c(1.0, 1.04, 1.03, 1.06, 1.18, 1.30), 
       base = func_lastone(hh_nrgintst[["BY"]][["household"]][, "electricity"]), 
       showplot = set_showplot)
   } else if (grepl("SLC", set_scalc)) { 
