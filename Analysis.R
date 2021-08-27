3ec960b76e293817333e9ba0d588fb84a5f6782d
Kang <kangjf1943@gmail.com>
2021-08-27 16:52:47 +0900

    other: 改变情景名称
    去掉情景名称中过时的“OTHER”后缀。

--- Analysis.R
+++ Analysis.R
@@ -2,7 +2,7 @@
 # 计算内容或口径相关设置
 # 设置要计算的情景
 set_scalcs <- 
-  c("BAU", "BAU_WLC_OTHER", "BAU_SLCPLUS_OTHER", "BAU_SLC_DECOAL_OTHER")
+  c("BAU", "BAU_WLC", "BAU_SLCPLUS", "BAU_SLC_DECOAL")
 set_nrgplng_scope <- FALSE # 是否采用能源规划口径
 set_elecgensep <- TRUE # 是否将东亚电力从发电行业中独立
 
