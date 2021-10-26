7a2d24e313c2bd574ff74090761aea8daf6c68c8
Kang Jiefeng <kangjf1943@gmail.com>
2021-10-26 11:48:06 +0900

    fix: 调整减排情景参数
    目标是降低减排情景能耗强度五年下降率，和减煤情景区分开。

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,8 @@
 # SETTING ----
 # 计算内容或口径相关设置
 # 设置要计算的情景
-set_scalcs <- 
-  c("BAU", "BAU_WLC", "BAU_SLCPLUS", "BAU_SLC_DECOAL")
+set_scalcs <- "BAU_WLC"
+  # c("BAU", "BAU_WLC", "BAU_SLCPLUS", "BAU_SLC_DECOAL")
 set_nrgplng_scope <- FALSE # 是否采用能源规划口径
 set_elecgensep <- TRUE # 是否将东亚电力从发电行业中独立
 
@@ -1885,7 +1885,7 @@ for (set_scalc in set_scalcs) {
     #### WLC ----
     com_nrgintst[[set_scalc]][[1]] <- func_interp_3(
       year = c(2019, 2020, 2025, 2028, 2029, 2030, 2035, 2060), 
-      scale = c(1.0, 1.01, 1.04, 1.07, 1.07, 1.09, 1.10, 0.95), 
+      scale = c(1.0, 1.01, 1.05, 1.07, 1.07, 1.09, 1.10, 0.95), 
       base = func_lastone(com_nrgintst[["BY"]]$electricity$electricity), 
       "electricity", showplot = set_showplot)
   } else { 
@@ -2072,7 +2072,7 @@ for (set_scalc in set_scalcs) {
     #### WLC ----
     hh_nrgintst[[set_scalc]][[1]] <- func_interp_3(
       year = c(2019, 2020, 2025, 2029, 2030, 2035, 2060), 
-      scale = c(1.0, 1.01, 1.16, 1.28, 1.28, 1.43, 1.50), 
+      scale = c(1.0, 1.01, 1.17, 1.28, 1.29, 1.43, 1.50), 
       base = func_lastone(hh_nrgintst[["BY"]][["household"]][, "electricity"]), 
       showplot = set_showplot)
   } else { 
